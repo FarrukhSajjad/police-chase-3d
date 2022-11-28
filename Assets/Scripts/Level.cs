5c9924226e06d38f04fb4f8aa500ca8abe43c325
DESKTOP-7638QCR\OMEN <farrukhsajjad1998@gmail.com>
2022-11-28 20:47:08 +0500

    Text Mesh Pro and UI Placeholder

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -10,6 +10,12 @@ public class Level : MonoBehaviour
 
     public Transform playerSpawnPosition;
 
+    public bool showAdAfterThisLevel;
+
+    public int enemiesInThisLevel;
+
+    public int enemiesKilled = 0;
+
     public static Level Instance;
 
     private void Awake()
@@ -30,6 +36,11 @@ public class Level : MonoBehaviour
         //Spawn Player
         var player = Instantiate(playerPrefab, playerSpawnPosition.position, Quaternion.identity);
         LevelManager.Instance.objectFadeFollow.playerTransform = player.transform;
+        var levelNumber = LevelManager.Instance.currentLevelNumber + 1;
+        UIManager.Instance.levelNumberText.text = $"LEVEL {levelNumber}";
+
+        enemiesInThisLevel = protestors.Count;
+        UIManager.Instance.totalEnemiesToKillText.text = $"{enemiesKilled}/{enemiesInThisLevel}";
     }
 
     private void Update()
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI levelNumberText;
+    public TextMeshProUGUI totalEnemiesToKillText;
+
+    [Space(10)]
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject gameplayPanel;
+
+    [Space(10)]
+    [Header("Buttons")]
+    public Button playButton;
+
+    public static UIManager Instance;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            return;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playButton.onClick.AddListener(() =>
+               {
+                   OnPlayButtonPressed();
+               });
+    }
+
+    private void OnPlayButtonPressed()
+    {
+        mainMenuPanel.SetActive(false);
+        gameplayPanel.SetActive(true);
+
     }
 }
